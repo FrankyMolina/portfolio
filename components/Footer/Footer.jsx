45a2e7fad5f843440da1bf1e45a05cd2212b252e
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-09-16 01:14:40 +0200

    Navbar modified, ContactInfo component created, customClass on contact route send by props, oder minor changes

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,37 +1,11 @@
+import ContactInfo from '../ContactInfo';
+
 import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.contact}>
-        <div>
-          <p>
-            <a
-              href="https://www.linkedin.com/in/franciscojaviermolinatorrejon/"
-              target="_blank"
-            >
-              <img src="./linkedin logo.png" alt="linkedin logo" />
-              LinkdIn
-            </a>
-          </p>
-        </div>
-        <div>
-          <p>
-            <a href="mailto:fjmolinatorrejon@gmail.com">
-              <img src="./gmail logo.png" alt="gmail logo" />
-              Gmail
-            </a>
-          </p>
-        </div>
-        <div>
-          <p>
-            <a href="https://github.com/FrankyMolina" target="_blank">
-              <img src="./github logo.png" alt="github logo" />
-              GitHub
-            </a>
-          </p>
-        </div>
-      </div>
+      <ContactInfo />
 
       <div className={styles.copy}>
         <span>Franky Molina Developer © 2020.</span>
