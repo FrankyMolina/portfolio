5d73df73111080492210c795ce87d0acc1925a73
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-10-06 15:07:44 +0200

    Navbar and home page SCSS done, global color variables imported where it is needed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Layout from '../components/Layout';
 import MyDescription from '../components/MyDescription';
 
-import styles from '../styles/index.module.scss';
-
 export default function Home() {
   return (
     <Layout>
